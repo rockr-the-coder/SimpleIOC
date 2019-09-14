0a1574971cc8cc03da01e0723e2a4286451f6abe
Rakesh Marigondanahalli Shivaji <AJ2X@eurofins.com>
2019-09-14 10:43:20 +0530

    Add project files.

---
+++ DemoIOC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoIOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
