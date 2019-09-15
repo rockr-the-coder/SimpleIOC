0a29c8ecd13fc2ff08f12b6a5add12c37b53f9d8
rockr-hackr <rakeshms17@yahoo.com>
2019-09-15 17:12:20 +0530

    program written

--- DemoIOC/Program.cs
+++ DemoIOC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DemoIOC
 {
@@ -6,7 +7,57 @@ namespace DemoIOC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var container = new Container();
+            //container.For<ILogger>().Use<FileLogger>(); //use this or the register types method.
+            container.RegisterTypes(typeof(ILogger), typeof(FileLogger));
+            var log=container.Resolve<ILogger>();
+            Console.WriteLine(log.GetType());
+
+            Console.ReadLine();
         }
+    }
+
+    internal class Container
+    {
+
+        Dictionary<Type, Type> _map = new Dictionary<Type, Type>();
+        Type _sourceType;
+        public Container()
+        {
+        }
+
+        internal Container For<TSource>()
+        {
+            _sourceType = typeof(TSource);
+            return this;
+        }
+
+        internal void RegisterTypes(Type TSource, Type TDestination)
+        {
+            _map.Add(TSource, TDestination);
+        }
+
+        internal object Resolve<TSource>()
+        {
+            return Activator.CreateInstance(_map[typeof(TSource)]);
+        }
+
+        internal Container Use<TDestination>()
+        {
+            _map.Add(_sourceType, typeof(TDestination));
+            return this;
+        }
+
+
+    }
+
+
+    public interface ILogger
+    {
+
+    }
+    public class FileLogger:ILogger
+    {
+
     }
 }
